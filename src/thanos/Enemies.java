9bb2e0a13b308148bf961ffbe2217e5d30f1f963
Wendy Su <wsu315@gmail.com>
2019-04-26 14:44:46 -0700

    aiya

--- src/thanos/Enemies.java
+++ src/thanos/Enemies.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 public class Enemies {
 	
 	private int startX, startY, health, speed;
-	private double shit;
+	private double sheit;
 	
 	public Enemies(int x, int y, int h, int s) {
 		startX = x;
