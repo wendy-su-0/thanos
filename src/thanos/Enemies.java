f181d3893a3465764c2b105935398486ebb942bd
Wendy Su <wsu315@gmail.com>
2019-04-26 14:38:03 -0700

    why

--- src/thanos/Enemies.java
+++ src/thanos/Enemies.java
@@ -1,8 +1,29 @@
 package thanos;
 
+import java.awt.Graphics;
+
 public class Enemies {
 	
-	public Enemies() {
+	private int startX, startY, health, speed;
+	
+	public Enemies(int x, int y, int h, int s) {
+		startX = x;
+		startY = y;
+		health = h;
+		speed = s;
+	}
+	
+	private boolean isHit() {
+		
+		return false;
+	}
+	
+	public void draw(Graphics g) {
 		
 	}
+	
+	private void move() {
+		
+	}
+
 }
