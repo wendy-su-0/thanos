6bf2903b5ccdce4c1ba33a21bcee18580ae43203
zhujo <zhujo@DESKTOP-QL7RGJN>
2019-04-30 14:14:23 -0700

    reee

--- src/thanos/Avengers.java
+++ src/thanos/Avengers.java
@@ -6,10 +6,15 @@ public class Avengers extends GameObject{
 	private int v;
 	private Image img;
 
-	public Avengers(int x, int y, Image i) {
+	public Avengers(int x, int y, int vel, Image i) {
 		super(x, y, i);
+		v = vel;
 	}
-
-	
+	private void draw(Graphics g) {
+		g.
+	}
+	private int findX(Robot r) {
+		
+	}
 
 }
