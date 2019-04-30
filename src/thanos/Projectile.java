855a5c525a85fdc90a26002930071f00d95438e8
Wendy Su <wsu315@gmail.com>
2019-04-30 15:00:31 -0700

    updates to projectiles

--- src/thanos/Projectile.java
+++ src/thanos/Projectile.java
@@ -2,5 +2,15 @@ package thanos;
 
 public class Projectile {
 	
-
+	int startX, startY, finishX, finishY, strength;
+	boolean hitEnemy = false;
+	
+	public Projectile() {
+			
+	}
+	
+	public void hit() {
+		hitEnemy = true;
+	}
+	
 }
