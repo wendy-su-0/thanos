e6031dd690dfe523ffaa2c9a23990b1a41d6c752
zhujo <zhujo@DESKTOP-QL7RGJN>
2019-05-02 13:56:15 -0700

    getimage w string

--- src/thanos/Avengers.java
+++ src/thanos/Avengers.java
@@ -4,16 +4,30 @@ import kareltherobot.Robot;
 import thanos.GameObject;
 
 import java.awt.*;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Avengers extends GameObject{
 	private int x, y;
 	private int v;
 	private Image img;
 	public Projectile p;
+	
+	private Image getImage(String imgName) {
+		try {
+			img = ImageIO.read(this.getClass().getResource(imgName));
 
-	public Avengers(int x, int y, int vel, Image i) {
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return img;
+	}
+
+	public Avengers(int x, int y, int vel, String str) {
 		super(x, y, i);
 		v = vel;
+		img = this.getImage(str);
 	}
 	private void draw(Graphics g) {
 		g.drawImage(img, x , y, 50, 50, null);
