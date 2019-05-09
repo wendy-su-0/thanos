dce892615cedc812f03b822ddbd9a8deeb3740fa
Shankar <Shankar@10.142.14.168>
2019-05-09 15:03:42 -0700

    leviathan

--- src/thanos/Leviathan.java
+++ src/thanos/Leviathan.java
@@ -1,5 +1,12 @@
 package thanos;
 
-public class Leviathan {
+import java.awt.Image;
 
+public class Leviathan extends Enemies{
+
+	public Leviathan(int x, int y, int h, int s, Image i) {
+		super(x, y, h, s, i);
+		
+	}
+	
 }
