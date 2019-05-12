983b9149e9033c84d235e2699a26ad16a72c6ac6
Wendy Su <wsu315@gmail.com>
2019-05-12 13:43:02 -0700

    made an enemies list

--- src/thanos/ThanosGame.java
+++ src/thanos/ThanosGame.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class ThanosGame {
 	public List<GameObject> gos = new ArrayList<>();
+	public List<Enemies> enemies = new ArrayList<>();
 	public IronMan im1;
 	public CaptainAmerica ca1;
 	public ThanosGame() {
@@ -13,6 +14,10 @@ public class ThanosGame {
 		ca1 = new CaptainAmerica(400,200);
 		gos.add(im1);
 		gos.add(ca1);
+	}
+	
+	public List<GameObject> returnEnemies(){
+		return enemies;
 	}
 	
 	
