7646e6afc3d01c8a43e6ef91651404469abebdf0
Wendy Su <wsu315@gmail.com>
2019-05-11 22:15:59 -0700

    cleaned up code so current x/y pixels are updated w/each move

--- src/thanos/Enemies.java
+++ src/thanos/Enemies.java
@@ -9,27 +9,37 @@ import javafx.scene.shape.Circle;
 
 public class Enemies extends GameObject {
 
+	//the final int needs to be changed i'm too lazy rn to figure out the pixels of each ave and st
+	static final int AVE_SQUARE_SIZE = 64, ST_SQUARE_SIZE = 64;
 	private Rectangle hitbox;
 	private int startX, startY, health, speed;
 	private Direction direction;
-	private int x,y;
+	private int currentX, currentY;
 	private boolean isDead = false;
 	private Image image;
 	Robot r;
 	
 
 
-
+	//the initial x and y are pixels. they are then converted to st/ave coordinates 
+	//and passed into the robot constructor
 	public Enemies(int x, int y, int h, int s, Image i) {
+		//gameobject xy in pxels
 		super(x, y, i);
 		startX = x;
+		currentX = x;
 		startY = y;
+		currentY = y;
 		health = h;
 		speed = s;
 		direction = East;
-		r = new Robot(startX, startY, direction, 100);
+		//the changed constructor passes in coordinates rather than pizels
+		r = new Robot(startX/this.AVE_SQUARE_SIZE, startY/this.ST_SQUARE_SIZE, direction, 100);
 	}
-
+	//when workng with the enemey class we need to be very clear is we are
+	//working with a ave/st x/y or a x/y in pixels
+	
+	
 	private void isHit(Projectile p) {
 		if(p.getHitBox().contains(hitbox)) {
 			health -= p.getStrengt();
@@ -41,11 +51,28 @@ public class Enemies extends GameObject {
 	
 	private void destroy() {
 		image = null;
-		
 	}
 
 	public void draw(Graphics g) {
-		g.drawImage(image, startX, startY, null);
+		g.drawImage(image, currentX, currentY, null);
+	}
+	
+	private void updateX() {
+		//updates the xLoc in pixels based off of the changed avenue
+		currentX = r.avenue()*this.AVE_SQUARE_SIZE;
+	}
+	
+	private void updateY() {
+		//updates the yLoc in pixels based off the changed street
+		currentY = r.street()*this.ST_SQUARE_SIZE;
+	}
+	
+	public int getX() {
+		return currentX;
+	}
+	
+	public int getY() {
+		return currentY;
 	}
 
 	private void move() {
@@ -54,6 +81,8 @@ public class Enemies extends GameObject {
 		if(direction != East) {
 			if(r.frontIsClear()) {
 				r.move();
+				this.updateX();
+				this.updateY();
 			}
 		}
 
