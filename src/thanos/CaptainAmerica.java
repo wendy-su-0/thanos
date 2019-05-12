16fffac626c5b3f25d401ce52ea3c8afe13124c2
Wendy Su <wsu315@gmail.com>
2019-05-12 12:59:45 -0700

    they have the updated construcor with a radius for circle range

--- src/thanos/CaptainAmerica.java
+++ src/thanos/CaptainAmerica.java
@@ -3,7 +3,7 @@ package thanos;
 public class CaptainAmerica extends Avengers {
 
 	public CaptainAmerica(int X, int Y){
-	super(X, Y, 4, "captainamerica.png");
+	super(X, Y, 4, 50, "captainamerica.png");
 	
 	}
 }
