9032ca4b2b2cb3f7321cffffa748b17a951a6f3c
Verena <Verena@10.141.92.43>
2019-05-13 15:02:41 -0700

    runner changes

--- src/thanos/ThanosGameRunner.java
+++ src/thanos/ThanosGameRunner.java
@@ -22,7 +22,7 @@ public class ThanosGameRunner {
 	private ThanosGame game = new ThanosGame();
 	private Timer timer;
 	private int ticks;
-	private int r;
+	private int r1;
 	private int c;
 	private Image img = getImage();
 
@@ -121,7 +121,7 @@ public class ThanosGameRunner {
 	}
 	
 	protected void clickedAt(MouseEvent me) {
-		r = me.getX();
+		r1 = me.getX();
 		c = me.getY();
 		System.out.print(me);
 		panel.repaint();
