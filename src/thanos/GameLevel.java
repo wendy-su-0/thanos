920e00b69f2c6267f341a5ff73716dbad62cdbf3
Shankar <Shankar@LAPTOP-56UV7HSG>
2019-05-15 19:50:25 -0700

    eehhh

--- src/thanos/GameLevel.java
+++ src/thanos/GameLevel.java
@@ -9,7 +9,7 @@ public class GameLevel {
 	public int worldWidth = 8;
 	public int brickSize = 80;
 	public File worldselector;
-	Point st, junc1, junc2, junc3, end;
+	public static Point st, junc1, junc2, junc3, end;
 	
 	private Brick[] brick;
 
@@ -23,7 +23,6 @@ public class GameLevel {
 		junc2 = new Point(2149,2211);
 		junc3 = new Point(737,1399);
 		end = new Point(3261,1399);
-		
 	}
 	
 	public void draw(Graphics g) {
