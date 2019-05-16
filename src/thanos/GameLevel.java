fb7f2352ac60bab2dd85fcf52e54df73bff594d9
Shankar <Shankar@LAPTOP-56UV7HSG>
2019-05-15 19:49:46 -0700

    Points added for moving

--- src/thanos/GameLevel.java
+++ src/thanos/GameLevel.java
@@ -9,23 +9,24 @@ public class GameLevel {
 	public int worldWidth = 8;
 	public int brickSize = 80;
 	public File worldselector;
+	Point st, junc1, junc2, junc3, end;
 	
-	private Brick[][] bricks;
-	
+	private Brick[] brick;
+
 	public GameLevel() {
 		build();
 	}
-	
+
 	public void build() {
-		
-				
-		
-		
-		
+		st = new Point(564,2959);
+		junc1 = new Point(2336,719);
+		junc2 = new Point(2149,2211);
+		junc3 = new Point(737,1399);
+		end = new Point(3261,1399);
 		
 	}
 	
 	public void draw(Graphics g) {
-		
+	
 	}
 }
