d0aa9e22a0a00e21e44926ffb8d7053f72f389f9
Wendy Su <wsu315@gmail.com>
2019-05-23 11:49:40 -0700

    sdfdsf

--- src/thanos/ThanosGameRunner.java
+++ src/thanos/ThanosGameRunner.java
@@ -229,7 +229,7 @@ public class ThanosGameRunner {
 	}
 	
 	public void build() {
-		st = new Point((int) (0.16*ThanosGameRunner.WIDTH), (int)ThanosGameRunner.HEIGHT);
+		st = new Point((int) (0.16*ThanosGameRunner.WIDTH), (int) ThanosGameRunner.HEIGHT);
 		junc1 = new Point((int) (.639*ThanosGameRunner.WIDTH), (int) (.278*ThanosGameRunner.HEIGHT));
 		junc2 = new Point((int) (.597*ThanosGameRunner.WIDTH), (int) (.722*ThanosGameRunner.HEIGHT));
 		junc3 = new Point((int) (.231*ThanosGameRunner.WIDTH), (int) (.472*ThanosGameRunner.HEIGHT));
