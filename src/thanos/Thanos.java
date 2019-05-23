138016cc9688ae911e6a6a7c5d2f56638bb52afd
Verena <Verena@10.141.92.143>
2019-05-23 14:35:56 -0700

    changed the than man's pic

--- src/thanos/Thanos.java
+++ src/thanos/Thanos.java
@@ -5,13 +5,13 @@ import java.awt.Graphics;
 public class Thanos extends Enemies{
 	
 	public Thanos(int h, int s) {
-		super(h, s, "thanos.jpg");
+		super(h, s, "thanos.png");
 	}
 
 	@Override
 
 	public void draw(Graphics g) {
-		g.drawImage(image,currentX, currentY, 100,50, null);
+		g.drawImage(image,currentX, currentY, 100,100, null);
 	}
 
 }
