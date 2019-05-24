1594ccdfaf272e8d02e1c6ee4cb9b52b645d0e31
Wendy Su <wsu315@gmail.com>
2019-05-24 09:30:51 -0700

    new enemy!

--- src/thanos/Leviathan.java
+++ src/thanos/Leviathan.java
@@ -1,13 +1,20 @@
 package thanos;
 
-import java.awt.Image;
+import java.awt.Graphics;
 
 public class Leviathan extends Enemies{
 
 	public Leviathan(int h, int s) {
-		super(h, s, "thanos.jpg");
-		
+		super(h, s, "leviathan.jpg");
 	}
+
+	@Override
+
+	public void draw(Graphics g) {
+		//g.drawImage(image,currentX, currentY, size,size, null);
+		g.drawImage(image, currentX, currentY, size,size,null);
+	}
+
 	
 	
 }
